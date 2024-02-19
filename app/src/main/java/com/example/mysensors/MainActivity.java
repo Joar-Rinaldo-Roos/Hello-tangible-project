667cbd5ad75d98e9dd3d8edb8c9ae0e58565038e
Joar-Rinaldo-Roos <joar.rinaldo.roos@gmail.com>
2024-02-19 13:42:11 +0100

    imported ihave to fly package

--- app/src/main/java/com/example/mysensors/MainActivity.java
+++ app/src/main/java/com/example/mysensors/MainActivity.java
@@ -3,33 +3,63 @@ package com.example.mysensors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isMute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        // Button to open AcceleratorActivity
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AccelatorActivity.class);
-                MainActivity.this.startActivity(intent);  // Start the AcceleratorActivity
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
             }
         });
-        Button button2 = (Button) findViewById(R.id.button3);
-        button2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ShakeActivity.class);
-                MainActivity.this.startActivity(intent);  // Start the AcceleratorActivity
-                // Add actions for button2 here
+
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
+
+        isMute = prefs.getBoolean("isMute", false);
+
+        final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
+
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        else
+            volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+                else
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+
             }
         });
+
     }
 }
